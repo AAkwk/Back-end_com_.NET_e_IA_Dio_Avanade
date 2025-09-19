504b3ad0702289103755e47d13d761bd5bac3a6f
AAkwk <aakawakitas12@gmail.com>
2025-09-19 11:14:42 -0400

    update

---
+++ 02-Tipos_de_Operadores_em_C##/OperadorDeAtribuicao/Program.cs
@@ -0,0 +1,77 @@
+﻿//1)Introducao operador de atribuicao
+using System.Globalization;
+
+int a = 0;
+int b = 3;
+
+int c = a + b;
+
+Console.WriteLine(c);
+
+//2)Combinando operadores
+int d = 2;
+int e = 5;
+
+int f = d + e;
+
+//f = f + 5;
+
+f += 5;
+e -= 2;
+
+Console.WriteLine(f);
+
+Console.WriteLine(e);
+
+
+//3)Convertendo tipos de variaveis
+
+//  Cast - Casting
+int g = Convert.ToInt32("5");
+int h = int.Parse("5");
+
+Console.WriteLine(g);
+Console.WriteLine(h);
+
+//4) Diferenca entre Covert e Parse
+
+//O PARSE: todos os tipos pode ser covertido para outro tipo.
+//O tratamento de valores null. O CONVERT, ele aceita valores nulo, ja o Parse enceerra o programa.
+//e mas aceitavel utilizar o Convert
+
+//5) Conversao para sting
+int inteiro = 5;
+string i = inteiro.ToString();
+
+Console.WriteLine(i);
+
+//6) Cast implicito
+
+// int j = 5;
+// double k = j;
+
+// long j = long.MaxValue;
+// int k = Convert.ToInt32(j);
+
+int j = int.MaxValue;
+long k = j;
+
+
+Console.WriteLine(k);
+
+//7) Ordem dos operadores
+
+// double l = 4 / 2 + 2;
+double l = 4 / (2 + 2);
+
+
+Console.WriteLine(l);
+
+//8) Convertendo de maneira segura
+string m = "5-";
+
+int n = 0;
+int.TryParse(m, out n);
+
+Console.WriteLine(m);
+Console.WriteLine('Conversao realizada com sucesso!');
---
+++ 02-Tipos_de_Operadores_em_C##/OperadoresCondicionais/Program.cs
@@ -0,0 +1,95 @@
+﻿
+// using System.Numerics;
+// using System.Reflection.Metadata;
+// using System.Runtime.InteropServices;
+
+// int quantidadeDeEstoque = 10;
+// int quantidadeDeCompra = 0;
+// bool possivelVenda = quantidadeDeCompra > 0 && quantidadeDeEstoque >= quantidadeDeCompra ;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeDeEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeDeCompra}");
+// Console.WriteLine($"E possivel realizar a venda? {possivelVenda}");
+
+// if (quantidadeDeCompra == 0)
+// {
+//     Console.WriteLine("Venda Invalida!");
+// }
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe. Nao temos a quantidade desejada no estoque.");
+// }
+
+
+//2)switch case
+
+
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine();
+
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine($"E uma vogal");
+        break;
+
+    default:
+        Console.WriteLine($"Nao e uma vogal");
+        break;
+}
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u")
+// {
+//     Console.WriteLine($"E uma vogal");
+// }
+// else
+// {
+//      Console.WriteLine($"Nao e uma vogal");
+// }
+
+
+
+// if (letra == "a"))
+// {
+//     Console.WriteLine($"E uma vogal");
+// }
+// else if (letra == "a"))
+// {
+//   Console.WriteLine($"E uma vogal");  
+// }
+
+// else if (letra == "e"))
+// {
+//   Console.WriteLine($"E uma vogal");  
+// }
+
+// else if (letra == "i"))
+// {
+//   Console.WriteLine($"E uma vogal");  
+// }
+
+// else if (letra == "o"))
+// {
+//     Console.WriteLine($"E uma vogal");
+// }
+
+// else if (letra == "u"))
+// {
+//     Console.WriteLine($"E uma vogal");
+// }
+// else
+// {
+//     Console.WriteLine($"Nao e uma vogal");
+// }
