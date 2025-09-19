1536c8c7a246a582ad9867cc31094ae631e6b154
AAkwk <aakawakitas12@gmail.com>
2025-09-19 11:53:18 -0400

    update

---
+++ 02-Tipos_de_Operadores_em_C##/OperadoresLogicos/Program.cs
@@ -0,0 +1,45 @@
+﻿// 1) OR
+
+// bool eMaiorDeIdade = false;
+// bool possuiAutorizacaoDoResponsavel = false;
+
+// if (eMaiorDeIdade || possuiAutorizacaoDoResponsavel)
+// {
+//     Console.WriteLine("Entrada liberada!");
+// }
+// else
+// {
+//     Console.WriteLine("Entrada nao liberada!");
+// }
+
+
+
+
+// 2) AND
+
+// bool possuiPresencaMinima = false;
+// double media = 7.5;
+
+// if (possuiPresencaMinima && media >= 7)
+// {
+//     Console.WriteLine("Aprovado!");
+// }
+// else
+// {
+//     Console.WriteLine("Reprovado!");
+// }
+
+
+//3) NOT
+
+bool choveu = false;
+bool estaTarde = false;
+
+if (!choveu && !estaTarde)
+{
+    Console.WriteLine("Vou pedalar!");
+}
+else
+{
+    Console.WriteLine("Vou pedalar um outro dia!");
+}
